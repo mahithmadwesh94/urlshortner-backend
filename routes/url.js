e7546b470d122687bba1f2025347b4a626dcf7a3
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-07-31 09:52:56 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
+const cors = require('cors');
+
+
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 const dburi = process.env.ATLAS_URI;
 const hosturi = process.env.HOST_URL;
 
 
-//shortner url
-router.route('/shortner').post(async (req, res) => {
+var corsOptions = {
+    origin: 'https://m-shotener.netlify.app',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.get('/shortner', cors(corsOptions), async (req, res) => {
     let client = await mongoClient.connect(dburi);
     try {
         let db = client.db('shortner');
@@ -37,15 +44,46 @@ router.route('/shortner').post(async (req, res) => {
     }
 })
 
+//shortner url
+router.post('/shortner', cors(corsOptions), (async (req, res) => {
+    let client = await mongoClient.connect(dburi);
+    try {
+        let db = client.db('shortner');
+        const existingUrl = await db.collection('urls').find({ url: req.body.url }).toArray();
+        if (existingUrl.length) {
+            res.status(409).json('Shortened URL already exists for this site: ' + existingUrl[0].short);
+        } else {
+            var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var result = '';
+            for (var i = 0; i < 7; i++) {
+                result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+            }
+            let newUrl = {
+                url: req.body.url,
+                code: result,
+                // short: `${hosturi}+${result}`
+            }
+            const saveData = await db.collection('urls').insertOne(newUrl);
+            if (saveData) {
+                res.status(200).send(`${hosturi}${result}`);
+            }
+        }
+
+    } catch (error) {
+        res.status(400).json('Error: ' + error);
+    } finally {
+        client.close();
+    }
+}))
+
 
 //redirect to url
-router.route('/:shortUrl').get(async (req, res) => {
+router.get('/:shortUrl', cors(corsOptions), (async (req, res) => {
     let client = await mongoClient.connect(dburi);
     let reqUrl = req.params.shortUrl;
     try {
         let db = client.db('shortner');
         let existingUrl = await db.collection('urls').find({ code: reqUrl }).toArray();
-        console.log(existingUrl);
         if (existingUrl.length) {
             res.status(200).json(existingUrl[0].url);
         } else {
@@ -57,6 +95,6 @@ router.route('/:shortUrl').get(async (req, res) => {
     } finally {
         client.close();
     }
-})
+}))
 
 module.exports = router;
